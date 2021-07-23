a93db43cdfd6cdb56c320762e5d25e3807ad145f
Enzo Arauco <enzoarauco@gmail.com>
2021-07-23 15:34:53 -0500

    completed TheProjects component

--- components/TheEducation.vue
+++ components/TheEducation.vue
@@ -1,9 +1,16 @@
 <template>
-  <v-container>
-    <p class="text-h6 text-center">
-      Educación
-    </p>
-  </v-container>
+  <v-card>
+    <v-card-text>
+      <p class="text-h4 text--primary">
+        IDAT
+      </p>
+      <p> marzo del 2018 - enero del 2021 </p>
+      <div class="text--primary text-justify text-body-1">
+        Escuela de Tecnologías de la Información <br>
+        Carrera: Desarrollo de Sistemas de Información
+      </div>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
--- components/TheProjects.vue
+++ components/TheProjects.vue
@@ -1,14 +1,94 @@
 <template>
   <v-container>
-    <p class="text-h6 text-center">
-      Proyectos
-    </p>
+    <v-row justify="center">
+      <v-expansion-panels popout>
+        <v-expansion-panel
+          v-for="(project, i) in projects"
+          :key="i"
+          hide-actions
+        >
+          <v-expansion-panel-header>
+            <v-row
+              align="center"
+              class="spacer"
+              no-gutters
+            >
+              <v-col
+                class="hidden-xs-only"
+                sm="5"
+                md="3"
+              >
+                <strong v-text="project.name" />
+              </v-col>
+
+              <v-col
+                v-if="project.excerpt"
+                class="grey--text text-truncate hidden-sm-and-down"
+              >
+                {{ project.excerpt }}
+              </v-col>
+            </v-row>
+          </v-expansion-panel-header>
+
+          <v-expansion-panel-content>
+            <v-divider />
+            <v-card-text v-text="project.body" />
+            <v-card-text>
+              <p class="text-subtitle-1">
+                Repositorios:
+              </p>
+              <ul v-for=" (repo, index) of project.repos" :key="index">
+                <li> <a :href="repo" target="_blanck"> {{ repo }} </a> </li>
+              </ul>
+            </v-card-text>
+            <v-card-text>
+              <p class="text-subtitle-1">
+                Tecnologías
+              </p>
+              <ul v-for="(tech, index) in project.techs" :key="index">
+                <li>
+                  <v-icon :color="tech.color">
+                    {{ tech.icon }}
+                  </v-icon>
+                  {{ tech.name }}
+                </li>
+              </ul>
+            </v-card-text>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-export default {
+import { db } from '@/plugins/firebase'
 
+export default {
+  data () {
+    return {
+      projects: []
+    }
+  },
+  created () {
+    this.getProjects()
+  },
+  methods: {
+    async getProjects () {
+      try {
+        const projects = []
+        const res = await db.collection('proyectos').get()
+        res.forEach((doc) => {
+          const project = doc.data()
+          projects.push(project)
+        })
+        this.projects = projects
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error)
+      }
+    }
+  }
 }
 </script>
 
